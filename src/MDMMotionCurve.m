e1d882b11f1ecdd3edf5c8746c8d243939ea097a
featherless <jverkoey@gmail.com>
2017-06-05 22:05:27 -0700

    Initial commit of motion interchange format. (#1)
    Includes support for the following types of curves: instant, bezier, and spring.
    
    Also includes support for repetition behavior.

---
+++ src/MDMMotionCurve.m
@@ -0,0 +1,25 @@
+/*
+ Copyright 2017-present The Material Motion Authors. All Rights Reserved.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+#import "MDMMotionCurve.h"
+
+MDMMotionCurve MDMMotionCurveMakeBezier(float p1x, float p1y, float p2x, float p2y) {
+  return _MDMBezier(p1x, p1y, p2x, p2y);
+}
+
+MDMMotionCurve MDMMotionCurveMakeSpring(float mass, float tension, float friction) {
+  return _MDMSpring(mass, tension, friction);
+}
