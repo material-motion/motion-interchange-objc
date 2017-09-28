4cef2ac2742f41f25059ca3e0d943c8781164bea
Jeff Verkoeyen <featherless@google.com>
2017-09-28 11:28:52 -0400

    Add MDMMotionCurveFromTimingFunction for creating motion curves from timing functions.

--- src/MDMMotionCurve.h
+++ src/MDMMotionCurve.h
@@ -76,6 +76,11 @@ FOUNDATION_EXTERN MDMMotionCurve MDMMotionCurveMakeBezier(float p1x, float p1y, 
     NS_SWIFT_NAME(MotionCurveMakeBezier(p1x:p1y:p2x:p2y:));
 // clang-format on
 
+// clang-format off
+FOUNDATION_EXTERN MDMMotionCurve MDMMotionCurveFromTimingFunction(CAMediaTimingFunction * _Nonnull timingFunction)
+    NS_SWIFT_NAME(MotionCurve(fromTimingFunction:));
+// clang-format on
+
 /**
  Creates a spring curve with the provided configuration.
 
